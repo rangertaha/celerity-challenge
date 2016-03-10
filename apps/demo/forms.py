2c470d00748f9af6f49f5e77302d80f3ce63fb05
rangertaha <tahasaidi@gmail.com>
2016-03-10 16:35:07 -0500

    pep8 fixes

--- apps/demo/forms.py
+++ apps/demo/forms.py
@@ -9,6 +9,7 @@ from .roman import RomanNumeral
 class RomanNumeralsForm(forms.Form):
     number = forms.IntegerField(label='Number', min_value=0, max_value=3999,
                                 help_text='Enter a number between 0 to 3999')
+
     def __init__(self, *args, **kwargs):
         super(RomanNumeralsForm, self).__init__(*args, **kwargs)
         self.fields['number'].widget.attrs.update({'class': 'form-control'})
