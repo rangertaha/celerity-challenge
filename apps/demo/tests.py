7c19bc0d6b632a8d5474dcd79cca467edc35fcfc
rangertaha <tahasaidi@gmail.com>
2016-03-10 17:05:42 -0500

    Added some tests

--- apps/demo/tests.py
+++ apps/demo/tests.py
@@ -1,7 +1,35 @@
-# Standard lib imports
 # Third party imports
-# Local imports
-
 from django.test import TestCase
 
-# Create your tests here.
+# Local imports
+from .roman import RomanNumeral
+
+
+class RomanNumeralTestCase(TestCase):
+    def setUp(self):
+        self.TEST_CASES = [
+            ('', 0),
+            ('VI', 6),
+            ('IX', 9),
+            ('XVIII', 18),
+            ('XIX', 19),
+            ('XXXVIII', 38),
+            ('XXXIX', 39),
+            ('XL', 40),
+            ('XCVIII', 98),
+            ('CCCLXXXVIII', 388),
+            ('CDXCIX', 499),
+            ('DCCCLXVII', 867),
+            ('MCMXCVIII', 1998),
+            ('MMMCMXCIX', 3999),
+        ]
+
+    def test_int_to_rom(self):
+        """Testing the conversion from int to roman numerals"""
+        for c, n in self.TEST_CASES:
+            self.assertEqual(RomanNumeral(n).rom, c)
+
+    def test_rom_to_int(self):
+        """Testing the conversion from int to roman numerals"""
+        for c, n in self.TEST_CASES:
+            self.assertEqual(RomanNumeral(c).int, n)
--- celeritychallenge/settings.py
+++ celeritychallenge/settings.py
@@ -105,16 +105,6 @@ STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
 )
 
-
-# Use nose to run all tests
-TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
-
-# Tell nose to measure coverage on the 'foo' and 'bar' apps
-NOSE_ARGS = [
-    '--with-coverage',
-    '--cover-package=celeritychallenge,apps',
-]
-
 LOGGING = {
     'version': 1,
     'disable_existing_loggers': False,
